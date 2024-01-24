21691c5a87cad7ff8e50b31feb9e66be1f0e78ce
Hsueh-Xue <867997466@qq.com>
2024-01-24 21:35:47 +0800

    fix contest

--- spider/codeforces_spider.py
+++ spider/codeforces_spider.py
@@ -24,7 +24,6 @@ def get_user_contest_info(username):
     if res:
         if res["status"] != "OK":
             return {"success": False, "last_time": 0, 'contest_cnt': 0}
-        res['result'].reverse()
         return res['result']
     return {}
 
